f730383614b7327a82104796392143ac4d304bca
austinjohnson6 <austin.johnson@anellophotonics.com>
2023-11-10 16:22:21 -0800

    fix version history

--- include/anello_ros_driver/version.h
+++ include/anello_ros_driver/version.h
@@ -15,11 +15,16 @@
 
 
 /*
-v1.0.2 : Added data-port and config port auto-connect
 
-v1.0.3 : Added odometer forwarding
+v1.1.3 : Fixed serial port connection on boot
 
-v1.1.0 : Push changes to main
+v1.1.0 : Feature List:
+            1. Added odometer forwarding
+            2. Added data-port and config port auto-connect
+            3. Add launch file and parameter support
+
+v1.0.0 : Initial basic feature set
+
 */
 
 #endif
